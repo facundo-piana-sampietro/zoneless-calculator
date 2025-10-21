0a85df008061bb885ff21d02f7669028a47e0a16
facundo-piana-sampietro <facusampi@hotmail.com>
2025-10-21 17:57:56 -0300

    Sección 3: Se da lógica a la calculadora. Faltan algunas validaciones y solución de errores.

--- src/app/calculator/components/calculator-button.component/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button.component/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, input, output, signal, viewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -7,11 +7,19 @@ import { ChangeDetectionStrategy, Component, HostBinding, input, ViewEncapsulati
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./calculator-button.component.css'],
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400'
+    class: 'border-r border-b border-emerald-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()'
   },
   //encapsulation: ViewEncapsulation.None
 })
 export class CalculatorButtonComponent { 
+
+public isPressed = signal(false);
+
+  public onClick = output<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+
   public isCommand = input(false, {
     transform: (value: boolean | string) =>
       typeof value === 'string' ? value === '' : value
@@ -22,12 +30,37 @@ export class CalculatorButtonComponent {
       typeof value === 'string' ? value === '' : value
   });
 
-  // @HostBinding('class.is-command') get commandStyle(){
-  //   return this.isCommand();
+  /*
+    Setea el estilo "w-2/4 al host (es decir, sobre la etiqueta de calculator-button) cuando
+    el input "isDoubleSize" devuelva "true".
+    No es lo recomendado, por eso se migra al atributo "host" del @Component
+  */
+  // @HostBinding('class.w-2/4') get commandStyle(){
+  //   return this.isDoubleSize();
   // }
 
-  @HostBinding('class.w-2/4') get commandStyle(){
-    return this.isDoubleSize();
+  handleClick (){
+    if (!this.contentValue()?.nativeElement){
+      return;
+    }
+
+    const value = this.contentValue()!.nativeElement.innerText;
+    this.onClick.emit(value.trim());
+  }
+
+  public keyboardPressedStyle(key: string){
+    if (!this.contentValue()) return;
+    
+    const value = this.contentValue()!.nativeElement.innerText;
+
+    if (value !== key) return
+
+    this.isPressed.set(true);
+      
+    setTimeout(() => {
+      this.isPressed.set(false);
+    },100)
+
   }
 
 
--- src/app/calculator/components/calculator.component/calculator.component.ts
+++ src/app/calculator/components/calculator.component/calculator.component.ts
@@ -1,15 +1,65 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, HostListener, inject, viewChildren } from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button.component/calculator-button.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
 
 @Component({
   selector: 'calculator',
   imports: [CalculatorButtonComponent],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)'
+  },
+
+  //Así se puede resolver el encapsulamiento de estilos para el botón de la calculadora.
   // styles: `
   //   .is-command {
   //   @apply bg-indigo-700  bg-opacity-20
   // }
   // `
 })
-export class CalculatorComponent { }
+export class CalculatorComponent {
+
+  #calculatorService = inject(CalculatorService);
+
+  // Es una forma de hacerlo, pero se usan sin () en el html. Por eso, para mantener homogeneo, 
+  // no lo vamos a hacer así
+  // get resultText() {
+  //   return this.#calculatorService.resultText;
+  // }
+
+  //Cada vez que cambie el valor de los signals manejados dentro, estos signals se van a modificar.
+  public resultText = computed( ()=> this.#calculatorService.resultText())
+  public subResultText = computed( ()=> this.#calculatorService.subResultText())
+  public lastOperator = computed( ()=> this.#calculatorService.lastOperator())
+
+  //Igual que viewChild, pero para más de un hijo. En este caso, tenemos muchos botones hijos. 
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+
+  handleClick (key: string){
+    this.#calculatorService.constructNumber(key)
+  }
+
+  //@HostListener('document:keyup', ['$event']) --> Ya no se usa, se utiliza en el atributo "host" 
+  // de la anotación @Component
+  handleKeyboardEvent(event: KeyboardEvent){
+    const key = event.key
+    
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      '*': 'x',
+      '/': '÷',
+      Enter: '=',
+    }
+
+    //Si hay una equivalencia, usamos esa. Caso contrario, usamos la tecla que llegó.
+    const keyValue = keyEquivalents[key] ?? key;
+
+    this.handleClick(keyValue)
+
+    this.calculatorButtons().forEach(button => {
+      button.keyboardPressedStyle(keyValue);
+    })
+  }
+}
